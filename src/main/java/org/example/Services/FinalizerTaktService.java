14be0d36550252525fbbf4be6ba8e674aea43f2f
Lenovo <dima.kozak2008@gmail.com>
2024-06-29 12:14:08 -0700

    EatChance mechanic added

--- src/main/java/org/example/Services/FinalizerTaktService.java
+++ src/main/java/org/example/Services/FinalizerTaktService.java
@@ -5,7 +5,7 @@ import org.example.Objects.Cell;
 
 public class FinalizerTaktService {
     public void completeTakt(Cell cell , Animal animal){
-        animal.setWeight(animal.getWeight() - animal.getWeight()/10);
+        animal.setWeight(animal.getWeight() - animal.getWeight()/5);
         if(animal.getWeight()<animal.getMinWeight())cell.getAliveObjects().remove(animal);
     }
 }
