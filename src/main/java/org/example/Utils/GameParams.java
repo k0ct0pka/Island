fc5507f38d1ab17c8492f021fb52dbe6b4769cfd
Lenovo <dima.kozak2008@gmail.com>
2024-07-01 15:20:51 -0700

    code refactor

--- src/main/java/org/example/Utils/GameParams.java
+++ src/main/java/org/example/Utils/GameParams.java
@@ -1,5 +1,6 @@
 package org.example.Utils;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import org.example.Services.ConfigReader;
 
@@ -7,6 +8,7 @@ public class GameParams {
     @Getter
     private static final ConfigReader reader = ConfigReader.getInstance();
     @Getter
+    @JsonProperty
     private int timeTaktMilis;
     private GameParams() {
     }
